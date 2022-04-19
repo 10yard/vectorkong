d96d867f342c34aaa50e69fd1ed4d0d62a0f39cb
10yard <jon123wilson@hotmail.com>
2022-04-19 17:35:13 +0100

    Close girder ends Add constants for key ram addresses

--- vectorkong/init.lua
+++ vectorkong/init.lua
@@ -18,16 +18,19 @@ local vectorkong = exports
 function vectorkong.startplugin()
 	local mame_version
 	local vector_count
-	local is_girders, level, mode, mode_old
-	
+	local is_girders, store_mode
+
+	-- Constants for RAM addresses
+	local MODE, STAGE, LEVEL = 0x600a, 0x6227, 0x6229
+
 	-- Options
 	local enable_zigzags = true
-	
+
 	-- Vector character library
+	local BR = 0xffff  -- break in a vector chain
 	local vector_lib = {}
-	local B = 0xffff  -- break in a vector chain
 	vector_lib[0x00] = {0,2,0,4,2,6,4,6,6,4,6,2,4,0,2,0,0,2} -- 0
-	vector_lib[0x01] = {0,0,0,6,B,B,0,3,6,3,5, 1} -- 1
+	vector_lib[0x01] = {0,0,0,6,BR,BR,0,3,6,3,5, 1} -- 1
 	vector_lib[0x02] = {0,6,0,0,5,6,6,3,5,0} -- 2
 	vector_lib[0x03] = {1,0,0,1,0,5,1,6,2,6,3,5,3,2,6,6,6,1} -- 3
 	vector_lib[0x04] = {0,5,6,5,2,0,2,7} -- 4
@@ -36,48 +39,44 @@ function vectorkong.startplugin()
 	vector_lib[0x07] = {6,0,6,6,0,2} -- 7
 	vector_lib[0x08] = {2,0,0,1,0,5,2,6,5,0,6,1,6,4,5,5,2,0} -- 8
 	vector_lib[0x09] = {0,1,0,4,2,6,5,6,6,5,6,1,5,0,4,0,3,1,3,6} -- 9
-	vector_lib[0x11] = {0,0,4,0,6,3,4,6,0,6,B,B,2,6,2,0}  -- A
+	vector_lib[0x11] = {0,0,4,0,6,3,4,6,0,6,BR,BR,2,6,2,0}  -- A
 	vector_lib[0x12] = {0,0,6,0,6,5,5,6,4,6,3,5,2,6,1,6,0,5,0,0}  -- B
 	vector_lib[0x13] = {1,6,0,5,0,2,2,0,4,0,6,2,6,5,5,6} -- C
 	vector_lib[0x14] = {0,0,6,0,6,4,4,6,2,6,0,4,0,0} -- D
-	vector_lib[0x15] = {0,5,0,0,6,0,6,5,B,B,3,0,3,4} -- E
-	vector_lib[0x16] = {0,0,6,0,6,6,B,B,3,0,3,5} -- F
+	vector_lib[0x15] = {0,5,0,0,6,0,6,5,BR,BR,3,0,3,4} -- E
+	vector_lib[0x16] = {0,0,6,0,6,6,BR,BR,3,0,3,5} -- F
 	vector_lib[0x17] = {3,4,3,6,0,6,0,2,2,0,4,0,6,2,6,6} -- G
-	vector_lib[0x18] = {0,0,6,0,B,B,3,0,3,6,B,B,0,6,6,6} -- H
-	vector_lib[0x19] = {0,0,0,6,B,B,0,3,6,3,B,B,6,0,6,6} -- I
+	vector_lib[0x18] = {0,0,6,0,BR,BR,3,0,3,6,BR,BR,0,6,6,6} -- H
+	vector_lib[0x19] = {0,0,0,6,BR,BR,0,3,6,3,BR,BR,6,0,6,6} -- I
 	vector_lib[0x1a] = {1,0,0,1,0,5,1,6,6,6} -- J
-	vector_lib[0x1b] = {0,0,6,0,B,B,3,0,0,6,B,B,3,0,6,6} -- K
+	vector_lib[0x1b] = {0,0,6,0,BR,BR,3,0,0,6,BR,BR,3,0,6,6} -- K
 	vector_lib[0x1c] = {6,0,0,0,0,5} -- L
 	vector_lib[0x1d] = {0,0,6,0,2,3,6,6,0,6}  -- M
 	vector_lib[0x1e] = {0,0,6,0,0,6,6,6} -- N
 	vector_lib[0x1f] = {1,0,5,0,6,1,6,5,5,6,1,6,0,5,0,1,1,0} -- O
 	vector_lib[0x20] = {0,0,6,0,6,5,5,6,3,6,2,5,2,0} -- P
-	vector_lib[0x21] = {1,0,5,0,6,1,6,5,5,6,2,6,0,4,0,1,1,0,B,B,0,6,2,3} -- Q
+	vector_lib[0x21] = {1,0,5,0,6,1,6,5,5,6,2,6,0,4,0,1,1,0,BR,BR,0,6,2,3} -- Q
 	vector_lib[0x22] = {0,0,6,0,6,5,5,6,4,6,2,3,2,0,2,3,0,6} -- R
 	vector_lib[0x23] = {1,0,0,1,0,5,1,6,2,6,4,0,5,0,6,1,6,4,5,5} -- S
-	vector_lib[0x24] = {6,0,6,6,B,B,6,3,0,3} -- T
+	vector_lib[0x24] = {6,0,6,6,BR,BR,6,3,0,3} -- T
 	vector_lib[0x25] = {6,0,1,0,0,1,0,5,1,6,6,6} -- U
 	vector_lib[0x26] = {6,0,3,0,0,3,3,6,6,6} -- V
 	vector_lib[0x27] = {6,0,2,0,0,1,4,3,0,5,2,6,6,6}  -- W
 	vector_lib[0x28] = {0,0,6,6,3,3,6,0,0,6} -- X
-	vector_lib[0x29] = {6,0,3,3,6,6,B,B,3,3,0,3} -- Y
+	vector_lib[0x29] = {6,0,3,3,6,6,BR,BR,3,3,0,3} -- Y
 	vector_lib[0x2a] = {6,0,6,6,0,0,0,6} -- Z
 	vector_lib[0x2b] = {0,0,1,0,1,1,0,1,0,0}  -- dot
 	vector_lib[0x2c] = {3,0,3,5} -- dash
 	vector_lib[0x2d] = {5,0,5,6} -- underscore
-	vector_lib[0x2e] = {4,3,4,3,B,B,2,3,2,3} -- colon
+	vector_lib[0x2e] = {4,3,4,3,BR,BR,2,3,2,3} -- colon
 	vector_lib[0x2f] = {5,0,5,6} -- Alt underscore
-	vector_lib[0x34] = {2,0,2,5,B,B,4,0,4,5} -- equals
+	vector_lib[0x34] = {2,0,2,5,BR,BR,4,0,4,5} -- equals
 	vector_lib[0x35] = {3,0,3,5} -- dash
-	vector_lib[0x44] = {0,5,4,5,4,7,2,7,0,8,B,B,2,5,2,7,B,B,4,10,1,10,0,11,0,12,1,13,4,13,B,B,
-		0,15,4,15,4,17,2,17,2,18,0,18,0,15,B,B,2,15,2,17,B,B,0,23,0,21,4,21,4,23,B,B,2,21,2,22,B,B,
-		0,25,4,25,0,28,4,28,B,B,0,30,4,30,4,32,3,33,1,33,0,32,0,30} -- rub / end
-	vector_lib[0x49] = {0,4,2,2,5,2,7,4,7,8,5,10,2,10,0,8,0,4,B,B,2,7,2,5,5,5,5,7} -- copyright
-	vector_lib[0x6c] = {2,0,2,4,3,5,4,4,5,5,6,4,6,0,2,0,B,B,4,4,4,0,B,B,3,7,2,8,2,11,3,12,5,12,6,11,6,8,5,7,3,7,B,B,
-		2,14,6,14,2,19,6,19,B,B,6,21,3,21,2,22,2,25,3,26,6,26,B,B,2,28,2,31,4,31,4,28,5,28,6,29,6,31, B,B,
-		6,-2,6,-5,-12,-5,-12,36,6,36,6,33,B,B,0,-3,-10,-3,-10,34,0,34,0,-3} -- bonus
+	vector_lib[0x44] = {0,5,4,5,4,7,2,7,0,8,BR,BR,2,5,2,7,BR,BR,4,10,1,10,0,11,0,12,1,13,4,13,BR,BR,0,15,4,15,4,17,2,17,2,18,0,18,0,15,BR,BR,2,15,2,17,BR,BR,0,23,0,21,4,21,4,23,BR,BR,2,21,2,22,BR,BR,0,25,4,25,0,28,4,28,BR,BR,0,30,4,30,4,32,3,33,1,33,0,32,0,30} -- rub / end
+	vector_lib[0x49] = {0,4,2,2,5,2,7,4,7,8,5,10,2,10,0,8,0,4,BR,BR,2,7,2,5,5,5,5,7} -- copyright
+	vector_lib[0x6c] = {2,0,2,4,3,5,4,4,5,5,6,4,6,0,2,0,BR,BR,4,4,4,0,BR,BR,3,7,2,8,2,11,3,12,5,12,6,11,6,8,5,7,3,7,BR,BR,2,14,6,14,2,19,6,19,BR,BR,6,21,3,21,2,22,2,25,3,26,6,26,BR,BR,2,28,2,31,4,31,4,28,5,28,6,29,6,31, B,BR,6,-2,6,-5,-12,-5,-12,36,6,36,6,33,BR,BR,0,-3,-10,-3,-10,34,0,34,0,-3} -- bonus
 	vector_lib[0x70] = {0,2,0,4,2,6,4,6,6,4,6,2,4,0,2,0,0,2} -- Alt 0
-	vector_lib[0x71] = {0,0,0,5,B,B,0,3,6,3,5, 1} -- Alt 1
+	vector_lib[0x71] = {0,0,0,5,BR,BR,0,3,6,3,5, 1} -- Alt 1
 	vector_lib[0x72] = {0,6,0,0,5,6,6,3,5,0} -- Alt 2
 	vector_lib[0x73] = {1,0,0,1,0,5,1,6,2,6,3,5,3,2,6,6,6,1} -- Alt 3
 	vector_lib[0x74] = {0,5,6,5,6,3,3,0,2,0,2,6} -- Alt 4
@@ -87,7 +86,7 @@ function vectorkong.startplugin()
 	vector_lib[0x78] = {2,0,1,0,0,1,0,5,1,6,4,0,5,0,6,1,6,4,5,5,4,5,2,0} -- Alt 8
 	vector_lib[0x79] = {0,1,0,4,2,6,5,6,6,5,6,1,5,0,4,0,3,1,3,6} -- Alt 9
 	vector_lib[0x80] = {0,2,0,4,2,6,4,6,6,4,6,2,4,0,2,0,0,2} -- Alt 0
-	vector_lib[0x81] = {0,0,0,5,B,B,0,3,6,3,5, 1} -- Alt 1
+	vector_lib[0x81] = {0,0,0,5,BR,BR,0,3,6,3,5, 1} -- Alt 1
 	vector_lib[0x82] = {0,6,0,0,5,6,6,3,5,0} -- Alt 2
 	vector_lib[0x83] = {1,0,0,1,0,5,1,6,2,6,3,5,3,2,6,6,6,1} -- Alt 3
 	vector_lib[0x84] = {0,5,6,5,6,3,3,0,2,0,2,6} -- Alt 4
@@ -98,19 +97,20 @@ function vectorkong.startplugin()
 	vector_lib[0x89] = {0,1,0,4,2,6,5,6,6,5,6,1,5,0,4,0,3,1,3,6} -- Alt 9
 	vector_lib[0x8a] = {0,0,6,0,2,3,6,6,0,6}  -- Alt M
 	vector_lib[0x8b] = {0,0,6,0,2,3,6,6,0,6}  -- Alt M
-	vector_lib[0x9f] = {2,0,0,2,0,13,2,15,5,15,7,13,7,2,5,0,2,0,B,B,5,3,5,7,B,B,5,5,2,5,B,B,2,8,5,8,4,10,5,12,2,12} -- TM
-	--vector_lib[0xb0] = {0,0,7,0,7,7,0,7,0,0,B,B,1,1,1,6} -- Block
-	vector_lib[0xb0] = {0,0,0,8,B,B,1,0,1,8,B,B,7,0,7,8,B,B,6,0,6,8} -- Block
+	vector_lib[0x9f] = {2,0,0,2,0,13,2,15,5,15,7,13,7,2,5,0,2,0,BR,BR,5,3,5,7,BR,BR,5,5,2,5,BR,BR,2,8,5,8,4,10,5,12,2,12} -- TM
+	vector_lib[0xb0] = {0,0,0,8,BR,BR,1,0,1,8,BR,BR,7,0,7,8,BR,BR,6,0,6,8} -- Block
 	vector_lib[0xb1] = {0,0,7,0,7,7,0,7,0,0} -- Box
 	vector_lib[0xb7] = {0,1,1,1,1,2,6,2,6,1,7,1,7,3,6,3,6,5,7,5,7,7,6,7,6,6,1,6,1,7,0,7,0,1} -- Rivet
-	vector_lib[0xdd] = {0,0,7,0,B,B,4,0,4,4,B,B,1,4,7,4,B,B,2,9,1,6,7,6,7,9,B,B,5,6,5,9,B,B,7,11,2,11,3,14,B,B,
-		3,16,7,16,7,18,6,19,5,18,5,16,B,B,7,22,5,21,B,B,3,21,3,21} -- Help (big H)
-	vector_lib[0xed] = {7,0,5,0,B,B,6,0,6,4,B,B,7,4,4,4,B,B,7,9,7,6,4,6,3,9,B,B,5,6,5,9,B,B,7,11,3,11,2,14,B,B,
-		1,16,7,16,7,19,3,19,3,16,B,B,7,22,2,21,B,B,0,20,0,21} -- Help (little H)
-	vector_lib[0xfb] = {5,1,6,2,6,5,5,6,4,6,2,3,B,B,0,3,0,3} -- question mark
+	vector_lib[0xdd] = {0,0,7,0,BR,BR,4,0,4,4,BR,BR,1,4,7,4,BR,BR,2,9,1,6,7,6,7,9,BR,BR,5,6,5,9,BR,BR,7,11,2,11,3,14,BR,BR,3,16,7,16,7,18,6,19,5,18,5,16,BR,BR,7,22,5,21,BR,BR,3,21,3,21} -- Help (big H)
+	vector_lib[0xed] = {7,0,5,0,BR,BR,6,0,6,4,BR,BR,7,4,4,4,BR,BR,7,9,7,6,4,6,3,9,BR,BR,5,6,5,9,BR,BR,7,11,3,11,2,14,BR,BR,1,16,7,16,7,19,3,19,3,16,BR,BR,7,22,2,21,BR,BR,0,20,0,21} -- Help (little H)
+	vector_lib[0xfb] = {5,1,6,2,6,5,5,6,4,6,2,3,BR,BR,0,3,0,3} -- question mark
 	vector_lib[0xfd] = {-1,0,7,0} -- vertical line
 	vector_lib[0xfe] = {0,0,7,0,7,7,0,7,0,0} -- cross
-	vector_lib[0xff] = {5,2,7,2,7,4,5,4,5,2,B,B,5,3,2,3,0,1,B,B,2,3,0,5,B,B,4,0,3,1,3,5,4,6} -- jumpman / stick man
+	vector_lib[0xff] = {5,2,7,2,7,4,5,4,5,2,BR,BR,5,3,2,3,0,1,BR,BR,2,3,0,5,BR,BR,4,0,3,1,3,5,4,6} -- jumpman / stick man
+	-- non character objects:
+	vector_lib["oilcan"] = {1,1,15,1,BR,BR,1,15,15,15,BR,BR,5,1,5,15,BR,BR,12,1,12,15,BR,BR,7,4,10,4,10,7,7,7,7,4,BR,BR,7,9,10,9,BR,BR, 7,13,7,11,10,11,BR,BR,15,0,16,0,16,16,15,16,15,0,BR,BR,1,0,0,0,0,16,1,16,1,0}
+	vector_lib["hammer"] = {5,0,7,0,8,1,8,8,7,9,5,9,4,8,4,1,5,0,BR,BR,4,4,0,4,0,5,4,5,BR,BR,8,4,9,4,9,5,8,5}
+	vector_lib["barrel"] = {3,0,12,0,15,2,15,7,12,9,3,9,0,7,0,7,0,2,3,0,BR,BR,1,2,1,7,BR,BR,14,2,14,7,BR,BR,2,3,13,3,BR,BR,2,6,13,6}
 
 	function initialize()
 		mame_version = tonumber(emu.app_version())
@@ -129,39 +129,37 @@ function vectorkong.startplugin()
 	function main()
 		if cpu ~= nil then
 			vector_count = 0
-			level = read(0x6229)
-			mode = read(0x600a)
-			is_girders = read(0x77bf, 240)
-			is_complete = mode_old == 22 and mode == 8
-			is_intro = mode == 7
+			is_girders = read(0x77bf, 0xf0)
+			is_complete = store_mode == 22 and read(MODE, 8)
+			store_mode = read(MODE)
+			is_intro = read(store_mode, 7)
 
 			cls()
-			if is_complete then write(0x6227, 1);write(0x6229, level+1) end  -- force girders stage for now
-			if is_intro then write(0x600a, 8) end  -- skip the intro/climb scene
+			if is_complete then write(STAGE, 1); write(LEVEL, read(LEVEL)+1) end  -- force girders stage for now
+			if is_intro then write(store_mode, 8) end  -- skip the intro/climb scene
 			if is_girders then draw_girders_stage() end
 			draw_vector_characters()
 
 			--debug_limits(1000)
 			debug_vector_count()
 
-			mode_old = mode
 		end
 	end
 
 	function draw_girders_stage()
 		-- 1st girder
-		draw_girder(  1,   0,   1, 111)  -- flat section
-		draw_girder(  1, 111,   8, 223)  -- sloped section
+		draw_girder(  1,   0,   1, 111, "R")  -- flat section
+		draw_girder(  1, 111,   8, 223, "L")  -- sloped section
 		draw_ladder(  8,  80,   8) -- broken ladder bottom
 		draw_ladder( 32,  80,   4) -- broken ladder top
 		draw_ladder( 13, 184,  17) -- right ladder
-		draw_oilcan(  8, 16)
+		draw_object("oilcan",  8, 16)
 
 		-- 2nd Girder
 		draw_girder( 41,   0,  29, 207)
 		draw_ladder( 46,  32,  17)  -- left ladder
 		draw_ladder( 42,  96,  25)  -- right ladder
-		draw_hammer( 56, 168)
+		draw_object("hammer", 56, 168)
 
 		-- 3rd Girder
 		draw_girder( 62,  16,  74, 223)
@@ -182,21 +180,21 @@ function vectorkong.startplugin()
 		draw_ladder(139,  88,  13)  -- broken ladder bottom
 		draw_ladder(160,  88,   5)  -- broken ladder top
 		draw_ladder(145, 184,  17)  -- right ladder
-		draw_hammer(148,  17)
+		draw_object("hammer", 148,  17)
 
 		-- 6th girder
-		draw_girder(165,   0, 165, 143)  -- flat section
-		draw_girder(165, 143, 161, 207)  -- sloped section
+		draw_girder(165,   0, 165, 143, "R")  -- flat section
+		draw_girder(165, 143, 161, 207, "L")  -- sloped section
 		draw_ladder(172,  64,  52)  -- left ladder
 		draw_ladder(172,  80,  52)  -- middle ladder
 		draw_ladder(172, 128,  21)  -- right ladder
-		draw_barrel(173, 0)
-		draw_barrel(173, 10)
-		draw_barrel(189, 0)
-		draw_barrel(189, 10)
+		draw_object("barrel", 173, 0)
+		draw_object("barrel", 173, 10)
+		draw_object("barrel", 189, 0)
+		draw_object("barrel", 189, 10)
 
 		-- Pauline's girder
-		draw_girder(193,  88, 193, 136)
+		draw_girder(193,  88, 193, 136, "L")
 	end
 
 	function vector(y1, x1, y2, x2)
@@ -211,7 +209,7 @@ function vectorkong.startplugin()
 		local _y, _x
 		if data then
 			for _i=1, #data, 2 do
-				if _y and _x and data[_i] ~= B and data[_i+1] ~= B and _y ~= B and _x ~= B then
+				if _y and _x and data[_i] ~= BR and data[_i+1] ~= BR and _y ~= BR and _x ~= BR then
 					vector(data[_i]+_offy, data[_i+1]+_offx, _y+_offy, _x+_offx)
 				end
 				_y, _x =data[_i], data[_i+1]
@@ -242,7 +240,7 @@ function vectorkong.startplugin()
 
 	function wobble()
 		-- random change of the vector offset
-		if not mac.paused then return math.random(-40, 60) / 100 else return 0 end -- random change of the vector offset
+		if not mac.paused then return math.random(-40, 60) / 100 else return 0 end
 	end
 
 	function cls()
@@ -252,34 +250,24 @@ function vectorkong.startplugin()
 
 	-- vector objects
 	-----------------
-	function draw_oilcan(y, x)
-		box(y+15,  x,  1,  16)  -- top lip
-		box(y,     x,  1,  16)  -- bottom lip
-		polyline({1,1,15,1,B,B,1,15,15,15,B,B,5,1,5,15,B,B,12,1,12,15,B,B,7,4,10,4,10,7,7,7,7,4,B,B,7,9,10,9,B,B,
-				  7,13,7,11,10,11}, y, x)
-	end
-
-	function draw_hammer(y, x)
-		polyline({5,0,7,0,8,1,8,8,7,9,5,9,4,8,4,1,5,0,B,B,4,4,0,4,0,5,4,5,B,B,8,4,9,4,9,5,8,5}, y, x)  -- hammer
-	end
-
-	function draw_barrel(y, x)
-		-- draw an upright/stacked barrel
-		polyline({3,0,12,0,15,2,15,7,12,9,3,9,0,7,0,7,0,2,3,0,B,B,1,2,1,7,B,B,14,2,14,7,B,B,2,3,13,3,B,B,
-				  2,6,13,6}, y, x)
+	function draw_object(name, y, x)
+		-- draw object from the vector library
+		polyline(vector_lib[name], y, x)
 	end
 
 	function draw_ladder(y, x, h)
 		-- draw a single ladder at given y, x position of given height in pixels
-		polyline({0,0,h,0,B,B,0,8,h,8},y,x)  -- left and right legs
+		polyline({0,0,h,0,BR,BR,0,8,h,8},y,x)  -- left and right legs
 		for i=0, h-2 do  -- draw rung every 4th pixel (skipping 2 pixels at bottom)
 			if i % 4 == 0 then vector(y+i+2, x, y+i+2, x+8) end
 		end
 	end
 
-	function draw_girder(y1, x1, y2, x2)
-		-- draw parallel vectors (offset by 7 pixels) to form a girder.
-		polyline({y1,x1,y2,x2,B,B,y1+7,x1,y2+7,x2})
+	function draw_girder(y1, x1, y2, x2, open)
+		-- draw girder at given y,x position.  Girders are based on parallel vectors (offset by 7 pixels).
+		polyline({y1,x1,y2,x2,BR,BR,y1+7,x1,y2+7,x2})
+		if not open or open ~= "L" then	polyline({y1,x1,y1+7,x1}) end
+		if not open or open ~= "R" then polyline({y2,x2,y2+7,x2}) end
 		if enable_zigzags then  -- Fill the girders with optional zig zags
 			local _zig = 4  -- zigzag width 4 or 8 works well
 			local _cnt = 0
@@ -308,11 +296,7 @@ function vectorkong.startplugin()
 	function read(address, comparison)
 		-- return data from memory address or boolean when the comparison value is provided
 		_d = mem:read_u8(address)
-		if comparison then
-			return _d == comparison
-		else
-			return _d
-		end
+		if comparison then return _d == comparison else return _d end
 	end
 
 	function write(address, value)
